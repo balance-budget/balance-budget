4f34cab1612e6231485869d8059132bae3bca6b6
Christiaan de Ridder <christiaan@channelengine.com>
2026-05-20 19:35:27 +0200

    feat(data): add logging for failed entity operations

--- src/Balance.Services/Logging/LoggerExtensions.cs
+++ src/Balance.Services/Logging/LoggerExtensions.cs
@@ -1,5 +1,30 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Balance.Services.Logging;
 
-public static partial class LoggerExtensions { }
+public static partial class LoggerExtensions
+{
+    [LoggerMessage(LogLevel.Error, "Failed to create {entity}")]
+    public static partial void FailedToCreateEntity(
+        this ILogger logger,
+        DbUpdateException ex,
+        string entity
+    );
+
+    [LoggerMessage(LogLevel.Error, "Failed to update {entity} {id}")]
+    public static partial void FailedToUpdateEntity(
+        this ILogger logger,
+        DbUpdateException ex,
+        string entity,
+        string id
+    );
+
+    [LoggerMessage(LogLevel.Error, "Failed to delete {entity} {id}")]
+    public static partial void FailedToDeleteEntity(
+        this ILogger logger,
+        DbUpdateException ex,
+        string entity,
+        string id
+    );
+}
