0379632c5263811468200105d5b743ad684a657c
Christiaan de Ridder <christiaan@channelengine.com>
2026-05-20 19:39:13 +0200

    Fix DateConverters taking DateTimeKind.Local as is

--- src/Balance.Data/Helpers/DateConverters.cs
+++ src/Balance.Data/Helpers/DateConverters.cs
@@ -5,12 +5,15 @@ namespace Balance.Data.Helpers;
 internal static class DateConverters
 {
     internal static readonly ValueConverter<DateTime, DateTime> UtcConverter = new(
-        v => v,
+        v => v.Kind == DateTimeKind.Utc ? v : v.ToUniversalTime(),
         v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
     );
 
     internal static readonly ValueConverter<DateTime?, DateTime?> UtcNullableConverter = new(
-        v => v,
+        v =>
+            v == null ? v
+            : v.Value.Kind == DateTimeKind.Utc ? v
+            : v.Value.ToUniversalTime(),
         v => v == null ? v : DateTime.SpecifyKind(v.Value, DateTimeKind.Utc)
     );
 }
