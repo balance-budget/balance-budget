e195ac61551e8cce635fbe3e074cf086ef04d2be
Christiaan de Ridder <christiaan@channelengine.com>
2026-05-20 19:52:18 +0200

    feat(jobs): add DB maintenance job

---
+++ src/Balance.Services/Jobs/DatabaseMaintenanceService.cs
@@ -0,0 +1,37 @@
+using Balance.Data;
+using Balance.Data.Helpers;
+using Balance.Services.Contracts;
+using Balance.Services.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace Balance.Services.Jobs;
+
+internal class DatabaseMaintenanceService : IDatabaseMaintenanceService
+{
+    private readonly ILogger<DatabaseMaintenanceService> _logger;
+    private readonly BalanceDbContext _dbContext;
+
+    public DatabaseMaintenanceService(
+        ILogger<DatabaseMaintenanceService> logger,
+        BalanceDbContext dbContext
+    )
+    {
+        _logger = logger;
+        _dbContext = dbContext;
+    }
+
+    public async Task Optimize(CancellationToken cancellationToken)
+    {
+        _logger.DatabaseOptimizationStarted(DateTimeOffset.Now);
+
+        // The commands below work for both SQLite and PostgreSQL
+
+        // Run VACUUM command to shrink the database file size
+        await _dbContext.Database.Vacuum(cancellationToken);
+
+        // Run ANALYZE command to update the statistics for the database
+        await _dbContext.Database.Analyze(cancellationToken);
+
+        _logger.DatabaseOptimizationFinished(DateTimeOffset.Now);
+    }
+}
--- src/Balance.Services/Jobs/JobsServiceCollectionExtensions.cs
+++ src/Balance.Services/Jobs/JobsServiceCollectionExtensions.cs
@@ -1,3 +1,6 @@
+using Balance.Configuration.Helpers;
+using Balance.Configuration.Options;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Quartz;
 
@@ -5,11 +8,16 @@ namespace Balance.Services.Jobs;
 
 internal static class JobsServiceCollectionExtensions
 {
-    public static IServiceCollection AddBalanceJobs(this IServiceCollection services) =>
+    public static IServiceCollection AddBalanceJobs(
+        this IServiceCollection services,
+        IConfiguration configuration
+    ) =>
         services
             .AddQuartz(c =>
             {
+                var config = configuration.GetSection<JobsOptions>();
                 c.SchedulerName = "Balance Scheduler";
+                c.ScheduleJobAt<MaintenanceJob>(config.MaintenanceSchedule);
             })
             .AddQuartzHostedService(c =>
             {
