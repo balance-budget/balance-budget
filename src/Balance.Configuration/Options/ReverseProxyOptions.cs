b226b027e0ceac4abdee65c5cde2a2527ce19748
Christiaan de Ridder <christiaan@channelengine.com>
2026-05-22 14:48:47 +0200

    Only trust known local IPs as specified in ReverseProxyOptions

---
+++ src/Balance.Configuration/Options/ReverseProxyOptions.cs
@@ -0,0 +1,10 @@
+using Balance.Configuration.Contracts;
+
+namespace Balance.Configuration.Options;
+
+public sealed class ReverseProxyOptions : IOptionsSection
+{
+    public static string Section => "ReverseProxy";
+
+    public required IReadOnlyList<string> KnownNetworks { get; init; }
+}
