fec237f1684c8dc26f1c694a13bf26282a744264
Christiaan de Ridder <christiaan@channelengine.com>
2026-05-23 14:46:43 +0200

    Add commnents to clarify code

--- src/Balance.Configuration/Helpers/HostEnvironmentExtensions.cs
+++ src/Balance.Configuration/Helpers/HostEnvironmentExtensions.cs
@@ -12,19 +12,15 @@ public static class HostEnvironmentExtensions
         Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER_FAST_MODE") == "true";
 
     /// <summary>
-    /// Checks if the application is running for integration testing
+    /// Checks if the application is running for integration testing.
     /// </summary>
-    /// <param name="hostEnvironment"></param>
-    /// <returns></returns>
     public static bool IsIntegrationTest(this IHostEnvironment hostEnvironment) =>
         hostEnvironment.IsEnvironment("IntegrationTest");
 
     /// <summary>
-    /// Checks if the application is running at design time
-    /// For example, when generating an OpenAPI document viaMicrosoft.Extensions.ApiDescription.Server
+    /// Checks if the application is running at design time — for example, when generating an
+    /// OpenAPI document via <c>Microsoft.Extensions.ApiDescription.Server</c>.
     /// </summary>
-    /// <param name="_"></param>
-    /// <returns></returns>
     public static bool IsDesignTime(this IHostEnvironment _) =>
         Assembly.GetEntryAssembly()?.GetName().Name == "GetDocument.Insider";
 }
